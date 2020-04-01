4f2641e178263a2556ba2d2b4c30910b31052941
seanwayland <seanwayland@gmail.com>
2020-04-01 13:10:13 -0400

    Update streamsTest.php

--- streamsTest.php
+++ streamsTest.php
@@ -21,7 +21,7 @@ global $filename;
 /* Database connection settings */
 $host = 'localhost';
 $user = 'seanwayland';
-$pass = 'Goberheim1$';
+$pass = #########
 $db = 'sean';
 $dbport = 3306;
 $mysqli = new mysqli($host,$user,$pass,$db,$dbport) or die($mysqli->error);
